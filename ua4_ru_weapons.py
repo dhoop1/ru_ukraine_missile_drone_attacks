e97b7a29236f98afe23521491c45407bca57472c
dhoop1 <dihoop23@gmail.com>
2024-12-05 21:03:30 -0700

    Update gspread auth

--- ua4_ru_weapons.py
+++ ua4_ru_weapons.py
@@ -70,7 +70,7 @@ import gspread
 
 # Authorize gspread with Google Cloud API (per https://docs.gspread.org/en/v6.1.3/oauth2.html#enable-api-access-for-a-project)
 
-credentials = eval(os.environ.getenv("GOOGLE_KAGGLE_CREDENTIALS"))
+credentials = eval(os.environ.get("GOOGLE_KAGGLE_CREDENTIALS"))
 gc = gspread.service_account_from_dict(credentials)
 
 # %%
