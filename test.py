c8142bb3f3a4c573e07bc6cb52730dde9bc545f6
dhoop1 <dihoop23@gmail.com>
2024-12-06 12:07:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,28 +3,42 @@ import os, subprocess
 subprocess.check_call(['pip', 'install', 'dash', 'plotly', 'dash-html-components', 'dash-bootstrap-components'])
 
 import dash
-from dash import html
+from dash import html, dash_table
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 check = os.environ.get('port')
 
-btn = dbc.Button("Button", id="button", color="primary", outline=True, size="sm")
-
 if check is None:
-    check = "Testing"
+    check = "0"
+
+details_table = html.Div([
+    html.H2("Missile & Drone Model Details"),
+    dbc.Button("Reset Sorting", id="reset-sorting-btn", color="primary", outline=True, size="sm"),
+    dash_table.DataTable(
+        id='details-table',
+        columns=['Col1'],
+        data=[[check]]
+    )
+])
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 
 app.layout = html.Div([
-    dbc.Row(html.H1(check)),
-    dbc.Row(btn)
+    dbc.Col(details_table)
 ])
 
 @app.callback(
+        Output('details-table', 'sort_by'),
         Input('button', 'n_clicks')
 )
-def empty_sorting(n_clicks):
+def empty_sorting1(sort_by):
+    if sort_by == 'hit':
+        pass
+    else:
+        pass
+
+def empty_sorting2(n_clicks):
     if n_clicks:
         return []  # Reset sort_by to an empty list to clear sorting
     return dash.no_update
