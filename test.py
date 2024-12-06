f98d09639924a967c2518f1cd883ca013f36261c
dhoop1 <dihoop23@gmail.com>
2024-12-06 11:06:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-import os
+import os, subprocess
+
+subprocess.check_call(['pip', 'install', 'dash', 'plotly', 'dash-html-components', 'dash-bootstrap-components'])
 
 check = os.environ.get('port')
 
-print(check)
+import dash, plotly, dash_html_components
+from dash import html
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+
+app.layout = html.div([check])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=10000)
