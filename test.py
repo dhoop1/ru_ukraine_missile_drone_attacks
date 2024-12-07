c1e66f5db8fe1824a6b2355faccf8cb8d1b0d2d6
dhoop1 <dihoop23@gmail.com>
2024-12-06 21:58:16 -0700

    Gspread testing

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import gspread
+from datetime import date
 
 gc = gspread.service_account()
 
-mad_ref = gc.open('csis_kaggle_ru_ukraine_attacks').worksheet('Introduction')
+intro_ref = gc.open('csis_kaggle_ru_ukraine_attacks').worksheet('Introduction')
 
-cell_last_updated = mad_ref.find("Last Updated")
-cell_mad = mad_ref.find("missile_attacks_daily")
+cell_last_updated = intro_ref.find("Last Updated")
+cell_mad = intro_ref.find("missile_attacks_daily")
 
-print(cell_mad.row, cell_last_updated.col)
+intro_ref.update_cell(cell_mad.row, cell_last_updated.col, date.today().strftime("%m/%d/%Y"))
+
+#print(isinstance(cell_mad.row, int), isinstance(cell_last_updated.col, int))
